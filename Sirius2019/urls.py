2e4f9931445f9b084ad22dcac0daa162e06fab88
Tlmonko <ktkozlov>
2019-09-28 20:02:20 +0500

    Adding image compression to thumbnails

--- Sirius2019/urls.py
+++ Sirius2019/urls.py
@@ -19,10 +19,9 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
     path('admin/', admin.site.urls),
     url(r'^', include('mainpage.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from PIL import Image
+from django.conf import settings
 
 
 def index(request):
@@ -7,4 +9,8 @@ def index(request):
         uploaded_file = request.FILES['picture']
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
+        image = Image.open(settings.MEDIA_ROOT + "/" + uploaded_file.name)
+        image.thumbnail((500, 500), Image.NEAREST)
+        image.save(settings.MEDIA_ROOT + "/" + uploaded_file.name.split(".")[0] + "_min" + "." +
+                   uploaded_file.name.split(".")[1])
     return render(request, "Html/mainpage.html")
