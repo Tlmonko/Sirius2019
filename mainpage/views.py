32cfad40fd5c10d4424323841555b9bfc9962e82
Tlmonko <ktkozlov>
2019-10-17 22:39:56 +0500

    Added a delete image function

--- mainpage/views.py
+++ mainpage/views.py
@@ -7,28 +7,32 @@ import os
 
 
 def index(request):
+    isBe = False
     if request.method == "POST":
         uploaded_file = request.FILES['picture']
         fs = FileSystemStorage()
-        fs.save(uploaded_file.name, uploaded_file)
-        image = Image.open(settings.MEDIA_ROOT + "/" + uploaded_file.name)
+        if os.path.isfile(settings.MEDIA_ROOT + "/" + uploaded_file.name):
+            isBe = True
+        else:
+            fs.save(uploaded_file.name, uploaded_file)
+            image = Image.open(settings.MEDIA_ROOT + "/" + uploaded_file.name)
 
-        width, height = image.size
-        side = min(width, height)
-        left, upper = (width - side) // 2, (height - side) // 2
-        right, lower = left + side, upper + side
-        print(left, upper, right, lower)
-        print(width, height)
-        image = image.crop((left, upper, right, lower))
+            width, height = image.size
+            side = min(width, height)
+            left, upper = (width - side) // 2, (height - side) // 2
+            right, lower = left + side, upper + side
+            print(left, upper, right, lower)
+            print(width, height)
+            image = image.crop((left, upper, right, lower))
 
-        image.thumbnail((300, 300))
-        image.save(settings.MEDIA_ROOT + "/" + ".".join(uploaded_file.name.split(".")[:-1]) + "_min" + "." +
-                   uploaded_file.name.split(".")[-1])
-        new_object_params = {"path": "/media/" + uploaded_file.name,
-                             "path_thumbnails": "/media/" + ".".join(uploaded_file.name.split(".")[:-1]) + "_min" + "." +
-                                                uploaded_file.name.split(".")[-1], "name": uploaded_file.name}
+            image.thumbnail((300, 300))
+            image.save(settings.MEDIA_ROOT + "/" + ".".join(uploaded_file.name.split(".")[:-1]) + "_min" + "." +
+                    uploaded_file.name.split(".")[-1])
+            new_object_params = {"path": "/media/" + uploaded_file.name,
+                                "path_thumbnails": "/media/" + ".".join(uploaded_file.name.split(".")[:-1]) + "_min" + "." +
+                                                    uploaded_file.name.split(".")[-1], "name": uploaded_file.name}
 
-        Picture.objects.create(**new_object_params)
+            Picture.objects.create(**new_object_params)
     elif request.method == "GET":
         if "image" in request.GET:
             print(request.GET)
@@ -41,4 +45,7 @@ def index(request):
                 os.remove(path)
                 os.remove(path_min)
     pictures = Picture.objects.all()
-    return render(request, "Html/mainpage.html", {"pictures_list": pictures})
+    if isBe:
+        return render(request, "Html/mainpage.html", {"pictures_list": pictures, "param": "Картинка с таким названием уже существует!"})
+    else:
+        return render(request, "Html/mainpage.html", {"pictures_list": pictures, "param": ""})
